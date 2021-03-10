b76958a74ac4dbabf55f6f25c1a08467b3f8a68d
Maxime Druart <maxime41010.druart@gmail.com>
2021-03-10 10:04:36 +0100

    :sparkles: updated code for client side packages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useRef, useState } from "react"
 import { Helmet } from "react-helmet"
-import LocomotiveScroll from "locomotive-scroll"
 
 import Header from "../components/Header"
 import Hero from "../components/homepage/Hero"
@@ -8,20 +7,38 @@ import About from "../components/homepage/About"
 import Services from "../components/homepage/Services"
 import Footer from "../components/Footer"
 import ContactSpinner from "../components/homepage/ContactSpinner"
+import useStore from "../../store"
 
 export default function Home() {
   const mainContainerRef = useRef(null)
   const [scroll, setScroll] = useState(null)
+  const canvasLoadStatus = useStore(state => state.canvasLoadStatus)
+
+  useEffect(() => {
+    // sadly this is kind of the best solution to stop the user from scrolling on load time AND hide scrollbar during the time
+    // where locomotivescroll isnt dynamically loaded yet
+    if (canvasLoadStatus.progress < 100) {
+      document.body.style.overflow = "hidden"
+      document.body.style.position = "fixed"
+    } else {
+      // gosh look away pls
+      setTimeout(() => {
+        document.body.style.overflow = "initial"
+        document.body.style.position = "static"
+      }, 300)
+    }
+  }, [canvasLoadStatus])
 
   useEffect(() => {
     if (mainContainerRef.current) {
-      // wierd stuff but we cant import this at the top level as this package can only run client side (needs access to window / document)
-      let s = new LocomotiveScroll()
-      s.smooth = true
-      s.init()
-      s.scrollbarContainer = mainContainerRef.current
-      s.el = mainContainerRef.current
-      setScroll(s)
+      import("locomotive-scroll").then(LocomotiveScroll => {
+        let s = new LocomotiveScroll.default()
+        s.smooth = true
+        s.init()
+        s.scrollbarContainer = mainContainerRef.current
+        s.el = mainContainerRef.current
+        setScroll(s)
+      })
     }
     return () => scroll && scroll.destroy()
     // eslint-disable-next-line react-hooks/exhaustive-deps
