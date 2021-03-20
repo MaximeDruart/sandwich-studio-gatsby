192704d4139f44f22d7f91a693234d30c77040a7
Maxime Druart <maxime41010.druart@gmail.com>
2021-03-20 18:20:37 +0100

    :arrow_up: updated dependencies

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -3,7 +3,6 @@ import React from "react"
 
 import GlobalStyle from "./styles/globalStyle"
 import theme from "./styles/defaultTheme"
-// import "./i18n"
 
 export const wrapRootElement = ({ element }) => (
   <ThemeProvider theme={theme}>
@@ -11,3 +10,6 @@ export const wrapRootElement = ({ element }) => (
     {element}
   </ThemeProvider>
 )
+
+// const GlobalStyle = require("./styles/globalStyle").default
+// const theme = require("./styles/defaultTheme").default
