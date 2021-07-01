a51648b0b681c2b68a2948a90eecc46b1063fc92
Lucien Chotard <61746435+chotardlucien@users.noreply.github.com>
2021-07-01 14:04:41 +0200

    luge fix

--- src/components/misc/Luge.js
+++ src/components/misc/Luge.js
@@ -1,16 +1,27 @@
 import React,{useRef,useState,useEffect} from "react"
-import luge from '@waaark/luge'
 export default function Luge({location,children}) {
     const mainContainerRef = useRef()
     let [isLoading,setIsLoading] = useState(true)
+    let [lugeModule,setLuge] = useState(null)
+    useEffect(()=>{
+      console.log('INIT')
+      import("@waaark/luge")
+      .then((luge) => {
+        setLuge(luge)
+      })
+      .catch((error) => console.error(error));
+    },[mainContainerRef])
     useEffect(() => {
         if(isLoading){
             window.setTimeout(()=>{
-              luge.lifecycle.refresh()
-              setIsLoading(false)
+              if(lugeModule!= null){
+                console.log(lugeModule)
+                lugeModule.default.lifecycle.refresh()
+                setIsLoading(false)
+              }
             },2000)
           }
-    }, [location,isLoading])
+    }, [location,isLoading,lugeModule])
     return (
         <div data-scroll-container ref={mainContainerRef} data-lg-smooth>
           {children}
